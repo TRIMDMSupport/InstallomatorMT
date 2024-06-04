1fa61c52ae1061152e2741ab9d97cc66979171c5
Bencs Kristóf <Bencs.Kristof@telekom.hu>
2024-06-04 15:54:35 +0200

    skippelt labelek törölve

--- fragments/labels/adobedigitaleditions.sh
+++
@@ -1,7 +0,0 @@
-adobedigitaleditions)
-    name="Adobe Digital Editions"
-    type="pkgInDmg"
-    downloadURL="https://adedownload.adobe.com/pub/adobe/digitaleditions/ADE_4.5_Installer.dmg"
-    appNewVersion="4.5"
-    expectedTeamID="JQ525L2MZD"
-    ;;
