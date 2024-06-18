acdce05b3a45e3e881ee990510687c75a92f9b06
Bencs Kristóf <Bencs.Kristof@telekom.hu>
2024-06-18 16:28:31 +0200

    figma label létrehozva

---
+++ fragments/labels/figma.sh
@@ -0,0 +1,8 @@
+figma)
+    name="Figma"
+    type="dmg"
+    archiveName="Figma-124.1.11.dmg"
+    downloadURL="https://desktop.figma.com/mac/Figma-124.1.11.dmg"
+    appNewVersion="124.1.11"
+    expectedTeamID="T8RA8NE3B7"
+    ;;
