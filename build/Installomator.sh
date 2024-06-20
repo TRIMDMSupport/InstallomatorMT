790beb2579a05af80d54a0da8f6579e105068169
Imre Balázs <balazs.imre@k00138614.kfki.corp>
2024-06-20 14:42:54 +0200

    Bruno issue

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1525,11 +1525,9 @@ bruno)
     type="dmg"
     appNewVersion="1.11.0"
     if [[ $(arch) == "arm64" ]]; then
-       # archiveName="bruno_[0-9.]*_arm64_mac.dmg"
-        downloadURL="https://github.com/usebruno/bruno/releases/download/v$appNewVersion/bruno_$appNewVersion_arm64_mac.dmg"
+        downloadURL="https://github.com/usebruno/bruno/releases/download/v$appNewVersion/bruno_1.11.0_arm64_mac.dmg"
     elif [[ $(arch) == "i386" ]]; then
-       # archiveName="bruno_[0-9.]*_x64_mac.dmg"
-        downloadURL="https://github.com/usebruno/bruno/releases/download/v$appNewVersion/bruno_$appNewVersion_x64_mac.dmg"
+        downloadURL="https://github.com/usebruno/bruno/releases/download/v$appNewVersion/bruno_1.11.0_x64_mac.dmg"
     fi
     expectedTeamID="W7LPPWA48L"
     ;;
