f7878f453ad0ef95399f5167473084490710fb9c
Imre Balázs <balazs.imre@k00131790.kfki.corp>
2024-06-25 11:21:55 +0200

    Label verzió frissítések

--- fragments/labels/iterm.sh
+++ fragments/labels/iterm.sh
@@ -1,8 +1,8 @@
 iterm)
     name="iTerm"
     type="zip"
-    downloadURL="https://iterm2.com/downloads/stable/iTerm2-3_4_23.zip"
-    appNewVersion="3.4.23"
+    downloadURL="https://iterm2.com/downloads/stable/iTerm2-3_5_2.zip"
+    appNewVersion="3.5.2"
     expectedTeamID="H7V7XYVQ7D"
     blockingProcesses=( iTerm2 )
     ;;
