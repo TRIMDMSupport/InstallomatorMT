ba68e22c473da0d1d2637f51ac82367449eaf53e
Imre Balázs <balazs.imre@macbookpro.kfki.corp>
2024-10-02 12:22:02 +0200

    supportcompanion new version:1.3.0.80829

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1738,7 +1738,7 @@ santa)
 supportcompanion)
     name="SupportCompanion"
     type="pkg"
-    appNewVersion="1.1.0.80790"
+    appNewVersion="1.3.0.80829"
     downloadURL="https://github.com/macadmins/SupportCompanion/releases/download/v${appNewVersion}/SupportCompanion-${appNewVersion}.pkg"
     expectedTeamID="T4SK8ZXCXG"
     ;;
--- fragments/labels/supportcompanion.sh
+++ fragments/labels/supportcompanion.sh
@@ -1,7 +1,7 @@
 supportcompanion)
     name="SupportCompanion"
     type="pkg"
-    appNewVersion="1.1.0.80790"
+    appNewVersion="1.3.0.80829"
     downloadURL="https://github.com/macadmins/SupportCompanion/releases/download/v${appNewVersion}/SupportCompanion-${appNewVersion}.pkg"
     expectedTeamID="T4SK8ZXCXG"
     ;;
