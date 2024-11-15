b45072f3f502195f285f22b80101b663e7588bd1
Imre Balázs <balazs.imre@k00138866.kfki.corp>
2024-11-15 16:32:35 +0100

    new label: raycast

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2024-10-02"
+VERSIONDATE="2024-11-15"
 
 # MARK: Functions
 
@@ -1713,6 +1713,14 @@ proximity)
     blockingProcesses=("Cisco Proximity")
     downloadURL="https://proximity.cisco.com/mac/Proximity.dmg"
     expectedTeamID="2E72E2E39X"
+    ;;
+raycast)
+    name="Raycast"
+    type="dmg"
+    downloadURL="https://raycast-releases.9a8adc978cc22e82f2df4dd5f45c4ef2.r2.cloudflarestorage.com/Raycast_v1.85.2_eee80146b97e0e62b5f7bb1438f7aab655e1be23_universal.dmg?response-content-disposition=attachment%3B%20filename%3D%22Raycast.dmg%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=a1110c3120096d801a69e290464c461e%2F20241115%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20241115T151453Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=52bd6905fe17167dc1111d166eb8cff07edf85f30648d250f5c715281207d40a"
+    #appNewVersion="$( curl -fsIL "https://www.raycast.com/download" | grep -i ^location | grep Raycast_ | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' )"
+    appNewVersion="1.85.2"
+    expectedTeamID="SY64MV22J9"
     ;;
 rectangle)
     name="Rectangle"
---
+++ fragments/labels/raycast.sh
@@ -0,0 +1,8 @@
+raycast)
+    name="Raycast"
+    type="dmg"
+    downloadURL="https://raycast-releases.9a8adc978cc22e82f2df4dd5f45c4ef2.r2.cloudflarestorage.com/Raycast_v1.85.2_eee80146b97e0e62b5f7bb1438f7aab655e1be23_universal.dmg?response-content-disposition=attachment%3B%20filename%3D%22Raycast.dmg%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=a1110c3120096d801a69e290464c461e%2F20241115%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20241115T151453Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=52bd6905fe17167dc1111d166eb8cff07edf85f30648d250f5c715281207d40a"
+    #appNewVersion="$( curl -fsIL "https://www.raycast.com/download" | grep -i ^location | grep Raycast_ | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' )"
+    appNewVersion="1.85.2"
+    expectedTeamID="SY64MV22J9"
+    ;;
