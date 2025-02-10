6129cfaa8c0ead6bbf528a8476120e3702607501
Balázs Imre <TRI_MDM_Support@telekom.hu>
2025-02-10 09:49:12 +0100

    Add Label: vlc_local

--- fragments/labels/vlc_local.sh
+++ fragments/labels/vlc_local.sh
@@ -3,7 +3,7 @@ vlc_local)
     type="dmg"
     appNewVersion="3.0.21"
     archiveName="vlc-${appNewVersion}-arm64.dmg"
-    downloadURL="/Users/Shared/vlc-${appNewVersion}-arm64.dmg"
+    downloadURL="file:///Users/Shared/vlc-${appNewVersion}-arm64.dmg"
     versionKey="CFBundleShortVersionString"
     expectedTeamID="75GAHG3SZQ"
     ;;
