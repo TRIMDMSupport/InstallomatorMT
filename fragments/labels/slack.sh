5ef9470a1247b9f7ca09dca67879a8965924a413
Balázs Imre <TRI_MDM_Support@telekom.hu>
2025-06-27 10:13:07 +0200

    new label: slack

---
+++ fragments/labels/slack.sh
@@ -0,0 +1,8 @@
+slack)
+    name="Slack"
+    type="dmg"
+    appNewVersion="4.44.65"
+    downloadURL="https://downloads.slack-edge.com/desktop-releases/mac/universal/${appNewVersion}/Slack-${appNewVersion}-macOS.dmg"
+    expectedTeamID="BQR82RBBHL"
+    #current download link: curl -fsIL "https://slack.com/ssb/download-osx-universal" | grep -i "^location"
+    ;;
