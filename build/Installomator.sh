60bbd82fed223140b1356cb40c55d5fd2d48f6ea
Balázs Imre <TRI_MDM_Support@telekom.hu>
2025-07-17 16:44:05 +0200

    modify label: vmwarehorizonclient

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1987,7 +1987,7 @@ vmwarehorizonclient)
     #fileName=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&dlgType=PRODUCT_BINARY&downloadGroup=${downloadGroup}" | grep -o '"fileName":"[^"]*"' | cut -d: -f2 | sed 's/"//g')
     downloadURL="https://download3.omnissa.com/software/CART26FQ1_MAC_2503/Omnissa-Horizon-Client-2503-8.15.0-14236092062.dmg"
     appNewVersion="8.15.0"
-    expectedTeamID="EG7KH642X6"
+    expectedTeamID="S2ZMFGQM93"
     ;;
 webex|\
 webexteams)
