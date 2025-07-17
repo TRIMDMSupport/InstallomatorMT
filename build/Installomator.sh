956e6f7898af003044f9998e749775c3a044bbca
Balázs Imre <TRI_MDM_Support@telekom.hu>
2025-07-17 09:59:49 +0200

    modify label: vmwarehorizonclient

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2025-07-10"
+VERSIONDATE="2025-07-17"
 
 # MARK: Functions
 
@@ -1985,8 +1985,8 @@ vmwarehorizonclient)
     type="pkgInDmg"
     #downloadGroup=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY" | grep -o '[^"]*_MAC_[^"]*')
     #fileName=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&dlgType=PRODUCT_BINARY&downloadGroup=${downloadGroup}" | grep -o '"fileName":"[^"]*"' | cut -d: -f2 | sed 's/"//g')
-    downloadURL="https://download3.omnissa.com/software/CART25FQ2_MAC_2406/VMware-Horizon-Client-2406-8.13.0-10025792799.dmg"
-    appNewVersion="8.13.0"
+    downloadURL="https://download3.omnissa.com/software/CART26FQ1_MAC_2503/Omnissa-Horizon-Client-2503-8.15.0-14236092062.dmg"
+    appNewVersion="8.15.0"
     expectedTeamID="EG7KH642X6"
     ;;
 webex|\
