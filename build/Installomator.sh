90ed87012a3866c27bf4ef0992d70e4c482f12a5
Balázs Imre <TRI_MDM_Support@telekom.hu>
2025-07-18 10:30:14 +0200

    modify label: vmwarehorizonclient

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2025-07-17"
+VERSIONDATE="2025-07-18"
 
 # MARK: Functions
 
@@ -1981,7 +1981,7 @@ vlc)
     ;;
 
 vmwarehorizonclient)
-    name="VMware Horizon Client"
+    name="Omnissa Horizon Client"
     type="pkgInDmg"
     #downloadGroup=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY" | grep -o '[^"]*_MAC_[^"]*')
     #fileName=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&dlgType=PRODUCT_BINARY&downloadGroup=${downloadGroup}" | grep -o '"fileName":"[^"]*"' | cut -d: -f2 | sed 's/"//g')
--- fragments/labels/vmwarehorizonclient.sh
+++ fragments/labels/vmwarehorizonclient.sh
@@ -1,5 +1,5 @@
 vmwarehorizonclient)
-    name="VMware Horizon Client"
+    name="Omnissa Horizon Client"
     type="pkgInDmg"
     #downloadGroup=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY" | grep -o '[^"]*_MAC_[^"]*')
     #fileName=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&dlgType=PRODUCT_BINARY&downloadGroup=${downloadGroup}" | grep -o '"fileName":"[^"]*"' | cut -d: -f2 | sed 's/"//g')
