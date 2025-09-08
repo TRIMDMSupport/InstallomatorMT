90528fed09d8205833b70e208643304a2091a1da
Balázs Imre <TRI_MDM_Support@telekom.hu>
2025-09-08 15:23:20 +0200

    modify label: microsoftedge new version: 140.0.3485.54

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2025-09-05"
+VERSIONDATE="2025-09-08"
 
 # MARK: Functions
 
@@ -1737,8 +1737,10 @@ microsoftedgeenterprisestable)
     name="Microsoft Edge"
     type="pkg"
     #MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
-    downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
-    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    appNewVersion="140.0.3485.54"
+    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/31b9aeb9-9251-4eda-8133-047734bfd5a1/MicrosoftEdge-140.0.3485.54.pkg"
+    # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
+    # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     expectedTeamID="UBF8T346G9"
     ;;
 microsoftexcel)
