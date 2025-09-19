c065f97c13d52b7fe3f71387dddbc66726f2da84
System Administrator <TRI_MDM_Support@telekom.hu>
2025-09-19 17:01:01 +0200

    bugfix

--- MDM/MT_Intune/MT_Intune_SW_Update.sh
+++ MDM/MT_Intune/MT_Intune_SW_Update.sh
@@ -4,7 +4,7 @@
 getVFAMDM () {
     # Ensure the target directory exists
     mkdir -p /usr/local/Installomator
-
+    rm -rf /usr/local/Installomator/MT_Intune_VFA.sh
     # Check when the script was last modified, if less than 10 minutes, then skip the download
     if [ -f "/usr/local/Installomator/MT_Intune_VFA.sh" ]; then
         if ! [ `find "/usr/local/Installomator/MT_Intune_VFA.sh" -mmin +30` ]; then
@@ -13,10 +13,21 @@ getVFAMDM () {
         fi
     fi
 
-    if ! curl -L -# --show-error 'https://github.com/TRIMDMSupport/InstallomatorMT/releases/latest/download/MT_Intune_VFA.sh' -o '/usr/local/Installomator/MT_Intune_VFA.sh' ; then
+    if ! curl -L -# --show-error 'https://raw.githubusercontent.com/TRIMDMSupport/InstallomatorMT/refs/heads/newLabels/MDM/MT_Intune/MT_Intune_VFA.sh' -o '/usr/local/Installomator/MT_Intune_VFA.sh' ; then
         echo "ERROR: Cannot download VFA MDM script."
     else
         chmod 755 /usr/local/Installomator/MT_Intune_VFA.sh
+    fi
+}
+
+getInstallomator () {
+    # Ensure the target directory exists
+    mkdir -p /usr/local/Installomator
+    rm -rf /usr/local/Installomator/Installomator.sh
+    if ! curl -L -# --show-error 'https://raw.githubusercontent.com/TRIMDMSupport/InstallomatorMT/refs/heads/newLabels/build/Installomator.sh' -o '/usr/local/Installomator/Installomator.sh' ; then
+        echo "ERROR: Cannot download Installomator script."
+    else
+        chmod 755 /usr/local/Installomator/Installomator.sh
     fi
 }
 # Directory to parse
@@ -24,15 +35,14 @@ DIRECTORY="/usr/local/Installomator/installed"
 # Script to call
 
 getVFAMDM
+getInstallomator
+getVFAMDM
+getInstallomator
 SCRIPT_TO_CALL="/usr/local/Installomator/MT_Intune_VFA.sh"
 # Iterate over each file in the directory
 for FILE in "$DIRECTORY"/*; do
   if [ -f "$FILE" ]; then
     FILENAME=$(basename "$FILE")
-    if [[ ! $FILENAME == *"_local"* ]]; then
-    FIRST_LINE=$(head -n 1 "$FILE")
-    # Call the script with the filename and first line as arguments
-    "$SCRIPT_TO_CALL" "$FILENAME" "$FIRST_LINE"
-    fi
+    "$SCRIPT_TO_CALL" "$FILENAME" "https://raw.githubusercontent.com/TRIMDMSupport/InstallomatorMT/refs/heads/newLabels/MDM/MT_Intune/Icons/${FILENAME}.png"
   fi
 done
