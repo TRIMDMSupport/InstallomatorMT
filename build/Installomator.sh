20a65af8b0136bb53e6b02b4e2506dd3c4c9304f
System Administrator <TRI_MDM_Support@telekom.hu>
2025-09-23 14:17:22 +0200

    espanso label modified

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1632,7 +1632,7 @@ espanso)
     appNewVersion="2.2.5"
     downloadURL="https://github.com/espanso/espanso/releases/download/v${appNewVersion}/Espanso-Mac-Universal.zip"
     blockingProcesses=( "Espanso" "espanso" )
-    expectedTeamID="K839T4T5BY"
+    expectedTeamID="6424323YUH"
     ;;
 evernote)
     name="Evernote"
--- fragments/labels/espanso.sh
+++ fragments/labels/espanso.sh
@@ -4,5 +4,5 @@ espanso)
     appNewVersion="2.2.5"
     downloadURL="https://github.com/espanso/espanso/releases/download/v${appNewVersion}/Espanso-Mac-Universal.zip"
     blockingProcesses=( "Espanso" "espanso" )
-    expectedTeamID="K839T4T5BY"
+    expectedTeamID="6424323YUH"
     ;;
