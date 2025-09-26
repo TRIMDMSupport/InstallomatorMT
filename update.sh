94384f10e89617eaff86b8f43655acb7fed364b7
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-09-26 08:52:56 +0200

    update.sh modified

--- update.sh
+++ update.sh
@@ -1,54 +1,80 @@
 #!/bin/bash
 
+# --- Színes kimenet beállítása ---
+COLOR_RED='\033[0;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_BLUE='\033[0;34m'
+COLOR_RESET='\033[0m'
+
+# --- Segédfüggvények ---
+
+log_info() {
+  echo -e "${COLOR_BLUE}[INFO]${COLOR_RESET} $1"
+}
+
+log_success() {
+  echo -e "${COLOR_GREEN}[SIKER]${COLOR_RESET} $1"
+}
+
+log_warn() {
+  echo -e "${COLOR_YELLOW}[FIGYELEM]${COLOR_RESET} $1" >&2
+}
+
+log_error() {
+  echo -e "${COLOR_RED}[HIBA]${COLOR_RESET} $1" >&2
+  exit 1
+}
+
 # 1. Bekér egy stringet a felhasználótól
-echo "Git Comment értéke: "
+log_warn "Git Comment értéke: "
 read -r comment
 
 # 2. Futtatja a ./utils/assemble.sh scriptet és vár 2 másodpercet
-echo "Futtatom a ./utils/assemble.sh scriptet..."
+log_info "Futtatom a ./utils/assemble.sh scriptet..."
 if [ -f "./utils/assemble.sh" ]; then
   ./utils/assemble.sh
   if [ $? -eq 0 ]; then
-    echo "A ./utils/assemble.sh script sikeresen lefutott."
+    log_success "A ./utils/assemble.sh script sikeresen lefutott."
   else
-    echo "Figyelem: A ./utils/assemble.sh script hibával fejeződött be."
+    log_error "Figyelem: A ./utils/assemble.sh script hibával fejeződött be."
   fi
 else
-  echo "Hiba: A ./utils/assemble.sh script nem található."
+  log_error "Hiba: A ./utils/assemble.sh script nem található."
 fi
 sleep 2
 
 # 3. Futtatja a git add . parancsot
-echo "Futtatom a 'git add .' parancsot..."
+log_info "Futtatom a 'git add .' parancsot..."
 git add .
 if [ $? -eq 0 ]; then
-  echo "'git add .' sikeresen lefutott."
+  log_success "'git add .' sikeresen lefutott."
 else
-  echo "Hiba történt a 'git add .' futtatása közben."
+  log_error "Hiba történt a 'git add .' futtatása közben."
   # Itt dönthetsz, hogy kilépsz-e vagy folytatod
   # exit 1
 fi
 
 # 4. Futtatja a git commit -m "a korábban bekért string értéke" parancsot
-echo "Futtatom a 'git commit -m \"$comment\"' parancsot..."
+log_info "Futtatom a 'git commit -m \"$comment\"' parancsot..."
 git commit -m "$comment"
 if [ $? -eq 0 ]; then
-  echo "'git commit' sikeresen lefutott."
+  log_success "'git commit' sikeresen lefutott."
 else
-  echo "Hiba történt a 'git commit' futtatása közben."
+  log_error "Hiba történt a 'git commit' futtatása közben."
   # Itt dönthetsz, hogy kilépsz-e vagy folytatod
   # exit 1
 fi
 
 # 5. Futtatja a git push parancsot
-echo "Futtatom a 'git push' parancsot..."
+log_info "Futtatom a 'git push' parancsot..."
 git push
 if [ $? -eq 0 ]; then
-  echo "'git push' sikeresen lefutott."
+  log_success "'git push' sikeresen lefutott."
 else
-  echo "Hiba történt a 'git push' futtatása közben."
+  log_error "Hiba történt a 'git push' futtatása közben."
   # Itt dönthetsz, hogy kilépsz-e vagy folytatod
   # exit 1
 fi
 
-echo "A script futása befejeződött."
+log_success "A script futása befejeződött."
