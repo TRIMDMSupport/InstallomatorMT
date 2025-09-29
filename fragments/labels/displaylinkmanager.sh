53588aa1b4897f227fc05c3815a797e75a312223
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-09-29 15:44:29 +0200

    űdisplaylinkmanager new version 13.1.0

--- fragments/labels/displaylinkmanager.sh
+++ fragments/labels/displaylinkmanager.sh
@@ -3,6 +3,6 @@ displaylinkmanager)
     type="pkgInZip"
     packageID="com.displaylink.DisplayLinkUserAgent"
     downloadURL="https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20Manager%20Graphics%20Connectivity13.1-EXE.zip"
-    appNewVersion="1.13.1"
+    appNewVersion="13.1.0"
     expectedTeamID="73YQY62QM3"
     ;;
