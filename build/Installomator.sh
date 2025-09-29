c43e6838ef2533791d3caacc11b7b5d799b871f7
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-09-29 14:42:04 +0200

    modify authpass

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1548,7 +1548,7 @@ authpass)
     name="AuthPass"
     type="zip"
     appNewVersion="1.9.11"
-    link="https://github.com/authpass/authpass/releases/download/v${appNewVersion}/AuthPass.app-${appNewVersion}_2007.zip/"
+    downloadURL="https://github.com/authpass/authpass/releases/download/v${appNewVersion}/AuthPass.app-${appNewVersion}_2007.zip/"
     expectedTeamID="64ZPC769JY"
     ;;
 bruno)
--- fragments/labels/authpass.sh
+++ fragments/labels/authpass.sh
@@ -2,6 +2,6 @@ authpass)
     name="AuthPass"
     type="zip"
     appNewVersion="1.9.11"
-    link="https://github.com/authpass/authpass/releases/download/v${appNewVersion}/AuthPass.app-${appNewVersion}_2007.zip/"
+    downloadURL="https://github.com/authpass/authpass/releases/download/v${appNewVersion}/AuthPass.app-${appNewVersion}_2007.zip/"
     expectedTeamID="64ZPC769JY"
     ;;
