fb2c56477c4be26bcacb91827f8f10765dc5af3a
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-10-03 22:49:08 +0200

    microsodtedgeit new version 141.0.3537.57

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1795,8 +1795,8 @@ microsoftedgeit)
     name="Microsoft Edge"
     type="pkg"
     #MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
-    appNewVersion="140.0.3485.54"
-    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/31b9aeb9-9251-4eda-8133-047734bfd5a1/MicrosoftEdge-140.0.3485.54.pkg"
+    appNewVersion="141.0.3537.57"
+    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/2aa012fa-b45e-4235-8fcf-9d42087a962c/MicrosoftEdge-141.0.3537.57.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     expectedTeamID="UBF8T346G9"
