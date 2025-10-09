1fbde619a74e6af4f8acc25ce53485ddd557fdbc
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-10-09 15:08:55 +0200

    microsoftedgesilver new version 141.0.3537.57

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1795,21 +1795,21 @@ microsoftedgeenterprisestable)
 microsoftedgeit)
     name="Microsoft Edge"
     type="pkg"
-    #MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
     appNewVersion="141.0.3537.57"
     downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/2aa012fa-b45e-4235-8fcf-9d42087a962c/MicrosoftEdge-141.0.3537.57.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    # MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
     expectedTeamID="UBF8T346G9"
     ;;
 microsoftedgesilver)
     name="Microsoft Edge"
     type="pkg"
-    #MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
-    appNewVersion="140.0.3485.54"
+    appNewVersion="141.0.3537.57"
     downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/31b9aeb9-9251-4eda-8133-047734bfd5a1/MicrosoftEdge-140.0.3485.54.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    # MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
     expectedTeamID="UBF8T346G9"
     ;;
 microsoftexcel)
--- fragments/labels/microsoftedgeIT.sh
+++ fragments/labels/microsoftedgeIT.sh
@@ -1,10 +1,10 @@
 microsoftedgeit)
     name="Microsoft Edge"
     type="pkg"
-    #MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
     appNewVersion="141.0.3537.57"
     downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/2aa012fa-b45e-4235-8fcf-9d42087a962c/MicrosoftEdge-141.0.3537.57.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    # MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
     expectedTeamID="UBF8T346G9"
     ;;
