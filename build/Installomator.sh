77916c3bab31393d5409cb6c2fbf6d47aa45d677
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-10-10 11:49:17 +0200

    microsoftcompanyportal label modified

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2025-10-09"
+VERSIONDATE="2025-10-10"
 
 # MARK: Functions
 
@@ -1763,14 +1763,14 @@ microsoftcompanyportal)
     name="Company Portal"
     type="pkg"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=853070"
-    appNewVersion="5.2508.0"
+    appNewVersion="$(curl -s "https://macupdater.net/app_updates/appinfo/com.microsoft.CompanyPortalMac/index.html" | grep -oE '<title>Mac App '\''Company Portal'\'' v[0-9]+\.[0-9]+\.[0-9]+' | sed -E "s/<title>Mac App 'Company Portal' v//g")"
     expectedTeamID="UBF8T346G9"
-    if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
-        printlog "Running msupdate --list"
-        "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
-    fi
-    updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
-    updateToolArguments=( --install --apps IMCP01 )
+    #if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
+    #    printlog "Running msupdate --list"
+    #    "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
+    #fi
+    #updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
+    #updateToolArguments=( --install --apps IMCP01 )
     ;;
 microsoftdefender|\
 microsoftdefenderatp)
--- fragments/labels/microsoftcompanyportal.sh
+++ fragments/labels/microsoftcompanyportal.sh
@@ -2,12 +2,12 @@ microsoftcompanyportal)
     name="Company Portal"
     type="pkg"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=853070"
-    appNewVersion="5.2508.0"
+    appNewVersion="$(curl -s "https://macupdater.net/app_updates/appinfo/com.microsoft.CompanyPortalMac/index.html" | grep -oE '<title>Mac App '\''Company Portal'\'' v[0-9]+\.[0-9]+\.[0-9]+' | sed -E "s/<title>Mac App 'Company Portal' v//g")"
     expectedTeamID="UBF8T346G9"
-    if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
-        printlog "Running msupdate --list"
-        "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
-    fi
-    updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
-    updateToolArguments=( --install --apps IMCP01 )
+    #if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
+    #    printlog "Running msupdate --list"
+    #    "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
+    #fi
+    #updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
+    #updateToolArguments=( --install --apps IMCP01 )
     ;;
