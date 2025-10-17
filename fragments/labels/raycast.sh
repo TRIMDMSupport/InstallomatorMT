8c2d974bbf5febc6527293fb6b050648a36b5535
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-10-17 16:57:43 +0200

    WIP: local changes

--- fragments/labels/raycast.sh
+++ fragments/labels/raycast.sh
@@ -3,6 +3,6 @@ raycast)
     type="dmg"
     downloadURL="https://releases.raycast.com/download"
     #appNewVersion="$( curl -fsIL "https://www.raycast.com/download" | grep -i ^location | grep Raycast_ | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' )"
-    appNewVersion="1.88.4"
+    appNewVersion="1.103.3"
     expectedTeamID="SY64MV22J9"
     ;;
