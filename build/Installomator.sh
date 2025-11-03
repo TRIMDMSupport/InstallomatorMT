2d1f05c36e1fd87a1eba0e4f77f4fcebd93dac27
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-11-03 09:44:51 +0100

    new version microsoftedgeit 142.0.3595.53

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2025-10-31"
+VERSIONDATE="2025-11-03"
 
 # MARK: Functions
 
@@ -1819,8 +1819,8 @@ microsoftedgeenterprisestable)
 microsoftedgeit)
     name="Microsoft Edge"
     type="pkg"
-    appNewVersion="141.0.3537.57"
-    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/2aa012fa-b45e-4235-8fcf-9d42087a962c/MicrosoftEdge-141.0.3537.57.pkg"
+    appNewVersion="142.0.3595.53"
+    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/4cbf3e59-5962-4785-b8d9-09e7c884dc30/MicrosoftEdgeBeta-142.0.3595.53.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     # MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
