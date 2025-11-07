b8af34dc7fa98b595c11ec6b16a6b91ab80853a0
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-11-07 14:10:41 +0100

    new label xmind

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2162,6 +2162,14 @@ wireshark)
     fi
     expectedTeamID="7Z6EMTD2C6"
     ;;
+xmind)
+    name="Xmind"
+    type="dmg"
+    downloadURL="https://xmind.app/zen/download/mac/"
+    appNewVersion="$(curl -fsIL $downloadURL | grep -i "^location" | grep -oE 'Xmind-for-macOS-(.+?)-[0-9]+\.dmg' | sed -E 's/Xmind-for-macOS-(.+)-[0-9]+\.dmg/\1/')"
+    expectedTeamID="4WV38P2X5K"
+    ;;
+
 xquartz)
     # credit: AP Orlebeke (@apizz)
     name="XQuartz"
