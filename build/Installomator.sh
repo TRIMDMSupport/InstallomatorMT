9c39bd3a820dfd4f9f5220c27049ce50078cbddf
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2025-11-24 21:22:45 +0100

    raycast new version 1.103.7

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2046,7 +2046,7 @@ raycast)
     type="dmg"
     downloadURL="https://releases.raycast.com/download"
     #appNewVersion="$( curl -fsIL "https://www.raycast.com/download" | grep -i ^location | grep Raycast_ | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' )"
-    appNewVersion="1.103.5"
+    appNewVersion="1.103.7"
     expectedTeamID="SY64MV22J9"
     ;;
 rectangle)
