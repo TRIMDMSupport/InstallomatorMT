eb34c7a2fbffb3b387b13f2ab0e59335c0ccdcfb
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-01-12 16:28:24 +0100

    podmancli label modified 5.7.1

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2004,7 +2004,7 @@ particulars)
 podmancli)
     name="Podman CLI"
     type="pkg"
-    appNewVersion="5.1.1"
+    appNewVersion="5.7.1"
     downloadURL="https://github.com/containers/podman/releases/download/v${appNewVersion}/podman-installer-macos-universal.pkg"
     archiveName="podman-installer-macos-universal.pkg"
     expectedTeamID="HYSCB8KRL2"
--- fragments/labels/podmancli.sh
+++ fragments/labels/podmancli.sh
@@ -1,7 +1,7 @@
 podmancli)
     name="Podman CLI"
     type="pkg"
-    appNewVersion="5.1.1"
+    appNewVersion="5.7.1"
     downloadURL="https://github.com/containers/podman/releases/download/v${appNewVersion}/podman-installer-macos-universal.pkg"
     archiveName="podman-installer-macos-universal.pkg"
     expectedTeamID="HYSCB8KRL2"
