9bbf9c221323a80e99e31bd09fc73dd06b2b23f3
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-01-12 15:53:59 +0100

    sqldeveloper modified

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2095,6 +2095,7 @@ sqldeveloper)
     type="zip"
     downloadURL="https://download.oracle.com/otn_software/java/sqldeveloper/sqldeveloper-24.3.1.347.1826-macos-aarch64.app.zip"
     appNewVersion="24.3.1.347"
+    shortVersion="24.3.1.347"
     expectedTeamID="VB5E2TV963"
     ;;
 stats)
