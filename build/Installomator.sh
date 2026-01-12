77ed04b84bae1f0055c003f22b8df168685cb4b4
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-01-12 15:30:33 +0100

    new label sqldeveloper

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2090,6 +2090,13 @@ slack)
     expectedTeamID="BQR82RBBHL"
     #current download link: curl -fsIL "https://slack.com/ssb/download-osx-universal" | grep -i "^location"
     ;;
+sqldeveloper)
+    name="SQLDeveloper"
+    type="zip"
+    downloadURL="https://download.oracle.com/otn_software/java/sqldeveloper/sqldeveloper-24.3.1.347.1826-macos-aarch64.app.zip"
+    appNewVersion="24.3.1"
+    expectedTeamID="VB5E2TV963"
+    ;;
 stats)
     name="Stats"
     type="dmg"
