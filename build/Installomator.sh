ae0f198916f9a29843b75dcd45c8c688ea461a00
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-01-19 13:10:58 +0100

    iterm label new version: 3.6.6

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1733,8 +1733,8 @@ icons)
 iterm)
     name="iTerm"
     type="zip"
-    downloadURL="https://iterm2.com/downloads/stable/iTerm2-3_5_3.zip"
-    appNewVersion="3.5.3"
+    downloadURL="https://iterm2.com/downloads/stable/iTerm2-3_6_6.zip"
+    appNewVersion="3.6.6"
     expectedTeamID="H7V7XYVQ7D"
     blockingProcesses=( iTerm2 )
     ;;
