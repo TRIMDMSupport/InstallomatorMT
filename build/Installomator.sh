42d1544cedb3948f473e4a2718f4b15ab665ae80
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-03-25 14:52:44 +0100

    new release omnissahorizonclient 8.17.1

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1987,8 +1987,8 @@ omnissahorizonclient)
     type="pkgInDmg"
     #downloadGroup=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY" | grep -o '[^"]*_MAC_[^"]*')
     #fileName=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&dlgType=PRODUCT_BINARY&downloadGroup=${downloadGroup}" | grep -o '"fileName":"[^"]*"' | cut -d: -f2 | sed 's/"//g')
-    downloadURL="https://download3.omnissa.com/software/CART26FQ2_MAC_2506/Omnissa-Horizon-Client-2506-8.16.0-16536825094.dmg"
-    appNewVersion="8.16.0"
+    downloadURL="https://download3.omnissa.com/software/CART26FQ4_MAC_2512.1/Omnissa-Horizon-Client-2512-8.17.1-22261165306.dmg"
+    appNewVersion="8.17.1"
     expectedTeamID="S2ZMFGQM93"
     ;;
 packages)
