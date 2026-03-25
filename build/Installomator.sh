895383f09a6e51f2aed5cecf7b62f367fb5b528b
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-03-25 16:05:31 +0100

    new release microsoftvisualstudiocode 1.112.0

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1955,7 +1955,7 @@ microsoftvisualstudiocode|\
 visualstudiocode)
     name="Visual Studio Code"
     type="zip"
-    appNewVersion="1.112"
+    appNewVersion="1.112.0"
     downloadURL="https://update.code.visualstudio.com/${appNewVersion}/darwin-universal/stable"
     expectedTeamID="UBF8T346G9"
     appName="Visual Studio Code.app"
