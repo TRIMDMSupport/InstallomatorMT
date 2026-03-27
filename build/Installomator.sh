e7e9ed27f395e42dbc333a476847601794b2c5ad
TRIMDMSupport <TRI_MDM_Support@telekom.hu>
2026-03-27 11:20:50 +0100

    new label nudge

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1982,6 +1982,14 @@ mongodbcompass)
     downloadURL="https://github.com/mongodb-js/compass/releases/download/v${appNewVersion}/mongodb-compass-${appNewVersion}-darwin-arm64.dmg" 
     expectedTeamID="4XWMY46275"
     ;;
+nudge)
+    name="Nudge"
+    type="pkg"
+    archiveName="Nudge-[0-9.]*.pkg"
+    appNewVersion="2.1.0.81852"
+    downloadURL="https://github.com/macadmins/nudge/releases/download/v${appNewVersion}/Nudge_Suite-${appNewVersion}.pkg"
+    expectedTeamID="T4SK8ZXCXG"
+    ;;
 omnissahorizonclient)
     name="Omnissa Horizon Client"
     type="pkgInDmg"
---
+++ fragments/labels/nudge.sh
@@ -0,0 +1,8 @@
+nudge)
+    name="Nudge"
+    type="pkg"
+    archiveName="Nudge-[0-9.]*.pkg"
+    appNewVersion="2.1.0.81852"
+    downloadURL="https://github.com/macadmins/nudge/releases/download/v${appNewVersion}/Nudge_Suite-${appNewVersion}.pkg"
+    expectedTeamID="T4SK8ZXCXG"
+    ;;
