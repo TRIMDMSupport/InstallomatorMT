b3410a6dc407fbfa38eb15255027be4e4a38fb0f
System Administrator <root@moj06d4j2klw.res.hu.corp>
2026-04-14 09:31:15 +0200

    new release displaylinkmanager 16.0.16

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1629,7 +1629,7 @@ displaylinkmanager)
     type="pkg"
     packageID="com.displaylink.DisplayLinkUserAgent"
     downloadURL="https://www.synaptics.com/sites/default/files/exe_files/2026-04/DisplayLink%20Manager%20Graphics%20Connectivity16.0-EXE.pkg"
-    appNewVersion="16.0.0"
+    appNewVersion="16.0.16"
     expectedTeamID="73YQY62QM3"
     ;;
 docker)
--- fragments/labels/displaylinkmanager.sh
+++ fragments/labels/displaylinkmanager.sh
@@ -3,6 +3,6 @@ displaylinkmanager)
     type="pkg"
     packageID="com.displaylink.DisplayLinkUserAgent"
     downloadURL="https://www.synaptics.com/sites/default/files/exe_files/2026-04/DisplayLink%20Manager%20Graphics%20Connectivity16.0-EXE.pkg"
-    appNewVersion="16.0.0"
+    appNewVersion="16.0.16"
     expectedTeamID="73YQY62QM3"
     ;;
