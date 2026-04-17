72b41f34c30856a6d58efd3ae5c8941b7a96e5e9
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-04-17 14:15:26 +0200

    new release microsoftvisualstudiocode 1.116.0

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2026-04-15"
+VERSIONDATE="2026-04-17"
 
 # MARK: Functions
 
@@ -1955,7 +1955,7 @@ microsoftvisualstudiocode|\
 visualstudiocode)
     name="Visual Studio Code"
     type="zip"
-    appNewVersion="1.115.0"
+    appNewVersion="1.116.0"
     downloadURL="https://update.code.visualstudio.com/${appNewVersion}/darwin-universal/stable"
     expectedTeamID="UBF8T346G9"
     appName="Visual Studio Code.app"
