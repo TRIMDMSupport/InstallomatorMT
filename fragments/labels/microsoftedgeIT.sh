d9b5789fce5256a1493e8c9c3f5ac8f0c7afebe6
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-04-20 10:54:13 +0200

    new release microsoftedgeit 147.0.3912.72

--- fragments/labels/microsoftedgeIT.sh
+++ fragments/labels/microsoftedgeIT.sh
@@ -1,8 +1,8 @@
 microsoftedgeit)
     name="Microsoft Edge"
     type="pkg"
-    appNewVersion="146.0.3856.84"
-    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/be10546a-4c12-4633-8dce-37a41a35fd5a/MicrosoftEdge-146.0.3856.84.pkg"
+    appNewVersion="147.0.3912.72"
+    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/aaafc949-a844-4aa0-8d18-193d14e9a385/MicrosoftEdge-147.0.3912.72.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     # MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
