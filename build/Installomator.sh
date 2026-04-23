6542a286b9d89ba6380313a954f183e3e01e2409
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-04-23 14:15:39 +0200

    espanso maybe ok

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1673,8 +1673,8 @@ dre_console)
 espanso)
     name="Espanso"
     type="appInDmgInZip"
-    downloadURL="$(downloadURLFromGit espanso espanso)"
-    appNewVersion="$(versionFromGit espanso espanso)"
+    downloadURL="https://github.com/espanso/espanso/releases/download/v2.3.0/Espanso-Mac-Universal.zip"
+    appNewVersion="2.3.0"
     blockingProcesses=( "Espanso" "espanso" )
     expectedTeamID="6424323YUH"
     ;;evernote)
--- fragments/labels/espanso.sh
+++ fragments/labels/espanso.sh
@@ -1,8 +1,8 @@
 espanso)
     name="Espanso"
     type="appInDmgInZip"
-    downloadURL="$(downloadURLFromGit espanso espanso)"
-    appNewVersion="$(versionFromGit espanso espanso)"
+    downloadURL="https://github.com/espanso/espanso/releases/download/v2.3.0/Espanso-Mac-Universal.zip"
+    appNewVersion="2.3.0"
     blockingProcesses=( "Espanso" "espanso" )
     expectedTeamID="6424323YUH"
     ;;
