d03726ef53ea5a028df866e5419ea111a881844c
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-05-04 10:28:44 +0200

    correct label notepadplusplus

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1979,7 +1979,7 @@ notepadplusplus)
     name="Notepad++"
     type="dmg"
     appNewVersion="1.0.5"
-    link="https://github.com/notepad-plus-plus-mac/notepad-plus-plus-macos/releases/download/v${appNewVersion}/Notepad++v${appNewVersion}.dmg"
+    downloadURL="https://github.com/notepad-plus-plus-mac/notepad-plus-plus-macos/releases/download/v${appNewVersion}/Notepad++v${appNewVersion}.dmg"
     expectedTeamID="S5972U9P85"
     ;;
 nudge)
