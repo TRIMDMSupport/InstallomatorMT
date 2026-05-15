898b5c935d5c18e3030302406c699a890224e332
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-05-15 13:45:44 +0200

    new release microsoftedgesilver 148.0.3967.54

--- fragments/labels/microsoftedgeSilver.sh
+++ fragments/labels/microsoftedgeSilver.sh
@@ -1,8 +1,8 @@
 microsoftedgesilver)
     name="Microsoft Edge"
     type="pkg"
-    appNewVersion="146.0.3856.84"
-    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/be10546a-4c12-4633-8dce-37a41a35fd5a/MicrosoftEdge-146.0.3856.84.pkg"
+    appNewVersion="148.0.3967.54"
+    downloadURL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/3aecaff4-a86f-4bbe-b8f8-daeac8f041b7/MicrosoftEdge-148.0.3967.54.pkg"
     # latest version downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     # latest version appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     # MS link older versions: https://www.microsoft.com/en-us/edge/business/download?form=MA13FJ
