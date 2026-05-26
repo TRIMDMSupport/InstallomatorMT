47710e34ae0dde90065a2cc13e78edebb8dbf2e8
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-05-26 11:01:08 +0200

    modify notepadplusplus label install allways latest version

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -1992,8 +1992,10 @@ mongodbcompass)
 notepadplusplus)
     name="Notepad++"
     type="dmg"
-    appNewVersion="1.0.5"
-    downloadURL="https://github.com/notepad-plus-plus-mac/notepad-plus-plus-macos/releases/download/v${appNewVersion}/Notepad++v${appNewVersion}.dmg"
+    #appNewVersion="1.0.5"
+    #downloadURL="https://github.com/notepad-plus-plus-mac/notepad-plus-plus-macos/releases/download/v${appNewVersion}/Notepad++v${appNewVersion}.dmg"
+    downloadURL=$(downloadURLFromGit notepad-plus-plus-mac notepad-plus-plus-macos)
+    appNewVersion=$(versionFromGit notepad-plus-plus-mac notepad-plus-plus-macos)
     expectedTeamID="S5972U9P85"
     ;;
 nudge)
