512ace3cdc4543c9f6bd68a91666f87492104fca
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-06-12 10:15:15 +0200

    Cyberduck label modified to install latest version

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -339,7 +339,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.6MT"
-VERSIONDATE="2026-06-03"
+VERSIONDATE="2026-06-12"
 
 # MARK: Functions
 
@@ -1622,9 +1622,9 @@ coconutbattery)
 cyberduck)
     name="Cyberduck"
     type="zip"
-    appNewVersion="9.3.1"
-    CFBundleVersion=".44136"
-    downloadURL="https://update.cyberduck.io//Cyberduck-${appNewVersion}${CFBundleVersion}.zip"
+    changeLOG="$( curl -fs https://version.cyberduck.io/changelog.rss )"
+    downloadURL=$( echo "$changeLOG" | sed 's/sparkle://g' | xmllint --xpath 'string(//item/enclosure/@url)' - 2>/dev/null)
+    appNewVersion=$( echo "$changeLOG" | sed 's/sparkle://g' | xmllint --xpath 'string(//item/enclosure/@shortVersionString)' - 2>/dev/null)
     expectedTeamID="G69SCX94XU"
     ;;
 dbeaverce)
--- fragments/labels/cyberduck.sh
+++ fragments/labels/cyberduck.sh
@@ -1,8 +1,8 @@
 cyberduck)
     name="Cyberduck"
     type="zip"
-    appNewVersion="9.3.1"
-    CFBundleVersion=".44136"
-    downloadURL="https://update.cyberduck.io//Cyberduck-${appNewVersion}${CFBundleVersion}.zip"
+    changeLOG="$( curl -fs https://version.cyberduck.io/changelog.rss )"
+    downloadURL=$( echo "$changeLOG" | sed 's/sparkle://g' | xmllint --xpath 'string(//item/enclosure/@url)' - 2>/dev/null)
+    appNewVersion=$( echo "$changeLOG" | sed 's/sparkle://g' | xmllint --xpath 'string(//item/enclosure/@shortVersionString)' - 2>/dev/null)
     expectedTeamID="G69SCX94XU"
     ;;
