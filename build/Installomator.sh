7ba3d7e65bd78d029033d839e6d2fec3cf851a9e
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-07-08 17:04:41 +0200

    modified miro label

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2037,6 +2037,7 @@ miro)
     # credit: @matins
     name="Miro"
     type="dmg"
+    appNewVersion=$(curl -fs https://formulae.brew.sh/api/cask/miro.json | sed -n 's/.*"version":"\([^"]*\)".*/\1/p')
     if [[ $(arch) == arm64 ]]; then
         downloadURL="https://desktop.miro.com/platforms/darwin-arm64/Install-Miro.dmg"
     elif [[ $(arch) == i386 ]]; then
