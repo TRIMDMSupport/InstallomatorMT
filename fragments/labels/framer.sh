42b80e8abd82d5706d754e2de3bfe25886619adb
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-07-09 17:35:18 +0200

    modified framer label

--- fragments/labels/framer.sh
+++ fragments/labels/framer.sh
@@ -1,7 +1,7 @@
 framer)
     name="Framer"
     type="zip"
-    appNewVersion="$(curl -fsSL "https://formulae.brew.sh/api/cask/framer.json" | sed -n 's/.*"version":"\([^"]*\)".*/\1/p')
+    appNewVersion="$(curl -fsSL "https://formulae.brew.sh/api/cask/framer.json" | sed -n 's/.*"version":"\([^"]*\)".*/\1/p')"
     downloadURL="https://updates.framer.com/electron/darwin/arm64/Framer.zip"
     expectedTeamID="JZ2M63CZ28"
     ;;
