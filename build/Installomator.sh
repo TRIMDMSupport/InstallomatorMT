30c6845c2e738972cda687278465eabc0950aa8e
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-07-10 15:00:24 +0200

    modified topnotch label

--- build/Installomator.sh
+++ build/Installomator.sh
@@ -2246,8 +2246,8 @@ swiftdialog)
     appNewVersion="$(versionFromGit swiftDialog swiftDialog)"
     expectedTeamID="PWA5E9TQ59"
     ;;
-topnoch)
-    name="Topnoch"
+topnotch)
+    name="Topnotch"
     type="AppInDmgInZip"
     appNewVersion=$(curl -fs https://formulae.brew.sh/api/cask/topnotch.json | sed -n 's/.*"version":"\([^"]*\)".*/\1/p')
     downloadURL="https://updates.topnotch.app/TopNotch-latest.zip"
--- fragments/labels/topnotch.sh
+++ fragments/labels/topnotch.sh
@@ -1,5 +1,5 @@
-topnoch)
-    name="Topnoch"
+topnotch)
+    name="Topnotch"
     type="AppInDmgInZip"
     appNewVersion=$(curl -fs https://formulae.brew.sh/api/cask/topnotch.json | sed -n 's/.*"version":"\([^"]*\)".*/\1/p')
     downloadURL="https://updates.topnotch.app/TopNotch-latest.zip"
