2b3c11bafb7ab111d0864fa381c702d6b8ecef93
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-07-14 11:41:26 +0200

    modify grammarly label

--- fragments/labels/grammarly.sh
+++ fragments/labels/grammarly.sh
@@ -4,6 +4,7 @@ grammarly)
      appNewVersion=$(curl -fsSL "https://formulae.brew.sh/api/cask/grammarly-desktop.json" | sed -n 's/.*"version":"\([^"]*\)".*/\1/p')
      packageID="com.grammarly.ProjectLlama"
      downloadURL="https://download-mac.grammarly.com/Grammarly.dmg"
+     versionKey="CFBundleVersion"
      expectedTeamID="W8F64X92K3"
      # appName="Grammarly Installer.app"
      installerTool="Grammarly Installer.app"
