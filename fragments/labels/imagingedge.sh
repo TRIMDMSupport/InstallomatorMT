7532f9c0fbd7b6461915e5ecfd8e423f445f880f
TRI_MDM_Support <TRI_MDM_Support@telekom.hu>
2026-07-14 14:42:39 +0200

    modify imagingedge label

--- fragments/labels/imagingedge.sh
+++ fragments/labels/imagingedge.sh
@@ -2,6 +2,6 @@ imagingedge)
     name="Imaging Edge Desktop"
     type="pkgInDmg"
     appNewVersion=$(curl -fsSL "https://formulae.brew.sh/api/cask/imaging-edge.json" | sed -n 's/.*"version":"\([^,"]*\).*/\1/p')
-    downloadURL="https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac\?fm\=gb"
+    downloadURL=$(curl -fs "https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac?fm=gb" | sed -n 's/.*href="\([^"]*\)".*/\1/p')
     expectedTeamID="VH49J2FYHE"
     ;;
